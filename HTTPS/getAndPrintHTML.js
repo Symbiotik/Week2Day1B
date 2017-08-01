ac887e5b05b4ce3bd376c44c06ec9c9446fdc0b5
Garnet <g.mesa@live.ca>
2017-08-01 00:52:21 +0000

    first commit

---
+++ HTTPS/getAndPrintHTML.js
@@ -0,0 +1,20 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+   var buffer = "";
+
+  https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      buffer += data;
+      console.log(buffer)
+    })
+  });
+}
+getAndPrintHTML();
---
+++ HTTPS/http-functions.js
@@ -0,0 +1,12 @@
+var https = require('https');
+module.exports = function getHTML (options, callback) {
+ https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    var buffer = "";
+    response.on('data', function (data) {
+      buffer += data;
+      callback(buffer);
+    });
+  });
+};
